6f225fa269a6150d7108e602d789338a4c602213
Muxa89 <muxa.89@inbox.ru>
2014-07-24 14:40:35 +0400

    Switched "type" field at sell page from text view to spinner.

--- src/com/altarix/test/activity/SellActivity.java
+++ src/com/altarix/test/activity/SellActivity.java
@@ -5,12 +5,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import com.altarix.test.R;
+import com.altarix.test.WareType;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * Created by mikhail on 22.07.14.
  */
 public class SellActivity extends Activity {
+
+    private static Spinner typesSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +28,14 @@ public class SellActivity extends Activity {
 
         setContentView(R.layout.sell);
 
-
+        typesSpinner = (Spinner) findViewById(R.id.typesSpinner);
+        ArrayList<String> typeNames = new ArrayList<String>();
+        for (WareType wareType : WareType.values()) {
+            typeNames.add(wareType.getName());
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(SellActivity.this, android.R.layout.simple_spinner_item, typeNames);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        typesSpinner.setAdapter(adapter);
     }
 
     public void toBuyPage(View view) {
